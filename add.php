fe092c5479aa351f8d5e3f547bb12f3a1bac7c51
domicb <domi1213@hotmail.com>
2016-02-11 20:54:06 +0100

    Mostramos bolas dentro de boton
    Avisar a moi sin falta a ver si nos lo quitamos rápido

--- add.php
+++ add.php
@@ -1,3 +1,9 @@
 <?php
-echo rand(0,90);
-?>
+/*$bolassacadas = json_decode($_GET['bolas']); //Convertimos el array bolas que está en json a php
+    
+do{
+    $bola = rand(1, 5);
+}while(in_array($bola, $bolassacadas));//Saca bola mientras esté guardado ya */
+
+echo rand(1,90);
+    
---
+++ ajax.js
@@ -0,0 +1,14 @@
+function SacaBola() {
+
+  if(bolassacadas.length == 5)
+    clearInterval(intervalo);
+  else{
+    var bolas = JSON.stringify(bolassacadas); //Convertimos el array bolassacadas en json 
+    $.get("numeros.php",{bolas: bolas}, MuestraBola); //Pasamos a php ese array
+  }
+} 
+
+function MuestraBola(bola){
+  bolassacadas.push(bola);//Añadimos la nueva bola sacada al array
+  document.getElementById('bola').innerHTML = bola;//La mostramos
+}
